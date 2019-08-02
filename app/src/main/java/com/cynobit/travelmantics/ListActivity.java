f509ddad9108d1d184cfeda9b1056a89b5b472d8
Francis Ilechukwu <francis94c@gmail.com>
2019-08-02 16:30:24 +0100

    ListActivity Menu and Refactor InsertActivity.

--- app/src/main/java/com/cynobit/travelmantics/ListActivity.java
+++ app/src/main/java/com/cynobit/travelmantics/ListActivity.java
@@ -1,7 +1,12 @@
 package com.cynobit.travelmantics;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -28,5 +33,23 @@ public class ListActivity extends AppCompatActivity {
         rvDeals.setAdapter(adapter);
         LinearLayoutManager dealsLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
         rvDeals.setLayoutManager(dealsLayoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.list_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.insert_menu:
+                Intent intent = new Intent(this, DealActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
